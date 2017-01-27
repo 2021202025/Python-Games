374ee3313042292e8755c01d053c031fa7c1027b
Manu <gmanu21097@gmail.com>
2017-01-27 19:18:25 +0530

    Added Snake

--- Snakiee.py
+++ Snakiee.py
@@ -5,6 +5,7 @@ pygame.init()
 white = (255,255,255)
 red = (255,0,0)
 black = (0,0,0)
+blue = (0,0,255)
 
 gameDisplay = pygame.display.set_mode((800,600))
 pygame.display.set_caption('Snakie')
@@ -20,6 +21,13 @@ while not gameExit:
             gameExit = True
         print(event)
     gameDisplay.fill(white)
+    pygame.draw.rect(gameDisplay, black, [400,300,10,50])
+    pygame.draw.rect(gameDisplay, red, [400,300,10,10])
+    pygame.draw.rect(gameDisplay, blue, [400,300,10,4])
+
+    gameDisplay.fill(red, rect=[200,200,50,10])
+
+    
     pygame.display.update()
 
 
