d5a4a7960ef4112ba75e99cd29baab9292fcd92a
Manu <gmanu21097@gmail.com>
2017-01-28 12:39:25 +0530

    Elegant Variables

--- Snakiee.py
+++ Snakiee.py
@@ -7,7 +7,10 @@ red = (255,0,0)
 black = (0,0,0)
 blue = (0,0,255)
 
-gameDisplay = pygame.display.set_mode((800,600))
+display_width = 800
+display_height = 600
+
+gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('Snakie')
 
 #pygame.display.flip()
@@ -15,12 +18,16 @@ pygame.display.set_caption('Snakie')
 
 gameExit = False
 
-lead_x = 300
-lead_y = 300
+lead_x = display_width/2
+lead_y = display_height/2
 lead_x_change = 0
 lead_y_change = 0
 
 clock = pygame.time.Clock()
+
+block_size = 10
+
+FPS = 60
 
 while not gameExit:
     for event in pygame.event.get():
@@ -29,33 +36,33 @@ while not gameExit:
             gameExit = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                lead_x_change = -2
+                lead_x_change = -block_size/5
                 lead_y_change = 0
             elif event.key == pygame.K_RIGHT:
-                lead_x_change = 2
+                lead_x_change = block_size/5
                 lead_y_change = 0
             elif event.key == pygame.K_UP:
-                lead_y_change = -2
+                lead_y_change = -block_size/5
                 lead_x_change = 0    
             elif event.key == pygame.K_DOWN:
-                lead_y_change = 2
+                lead_y_change = block_size/5
                 lead_x_change = 0
             
                 
-    if lead_x >= 800 or lead_x < 0 or lead_y >= 600 or lead_y < 0:
+    if lead_x >= display_width or lead_x < 0 or lead_y >= display_height or lead_y < 0:
         gameExit=True
                 
     lead_x += lead_x_change
     lead_y += lead_y_change
     
     gameDisplay.fill(black)
-    pygame.draw.rect(gameDisplay, red, [lead_x,lead_y,10,10])
+    pygame.draw.rect(gameDisplay, red, [lead_x,lead_y,block_size,block_size])
 
     #gameDisplay.fill(red, rect=[200,200,50,10])
 
     pygame.display.update()
     
-    clock.tick(60)
+    clock.tick(FPS)
 
 
 pygame.quit()
