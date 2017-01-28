1f9363ff37516a690671a87147608f32abaad51b
Manu <gmanu21097@gmail.com>
2017-01-28 11:16:23 +0530

    Did not mean to do that

--- Snakiee.py
+++ Snakiee.py
@@ -17,25 +17,26 @@ gameExit = False
 
 lead_x = 300
 lead_y = 300
-
+lead_x_change = 0
 while not gameExit:
     for event in pygame.event.get():
+        print(event)
         if event.type == pygame.QUIT:
             gameExit = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                lead_x -= 10
+                lead_x_change -= 10
             if event.key == pygame.K_RIGHT:
-                lead_x += 10
-        print(event)
+                lead_x_change += 10
+                
+    lead_x += lead_x_change
+     
     gameDisplay.fill(white)
     pygame.draw.rect(gameDisplay, red, [lead_x,lead_y,10,10])
 
     #gameDisplay.fill(red, rect=[200,200,50,10])
 
-    
     pygame.display.update()
-
 
 
 pygame.quit()
