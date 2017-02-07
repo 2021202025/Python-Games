74162e62e1520ee737fa127c5078a62995aeedf4
Manu <gmanu21097@gmail.com>
2017-02-07 13:01:18 +0530

    ZZzz...

--- Drawing.py
+++ Drawing.py
@@ -14,7 +14,13 @@ gameDisplay.fill(blue)
 
 Pix = pygame.PixelArray(gameDisplay)
 
-Pix[10:100][10:100] = red
+#Pix[200][300] = red
+
+pygame.draw.line(gameDisplay, red, (200,300), (500,500), 5)
+
+pygame.draw.circle(gameDisplay, red, (200,200), 100)
+
+pygame.draw.rect(gameDisplay, black, (150,150,200,100))
 
 while True:
     for event in pygame.event.get():
