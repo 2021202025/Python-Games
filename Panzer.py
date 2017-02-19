d444cdbe356bfc4710137b459490b1f4874f0dc9
Manu <gmanu21097@gmail.com>
2017-02-19 23:29:19 +0530

    Messed up Firing

--- Panzer.py
+++ Panzer.py
@@ -251,11 +251,9 @@ def explosion(x,y, size=50):
          
 
 def fireShell(xy,tankx,tanky,turPos,gun_power,xlocation,barrier_width,randomHeight):
+
     fire = True
-
     startingShell = list(xy)
-
-    
     print("FIRE",xy)
 
     while fire:
@@ -305,11 +303,59 @@ def fireShell(xy,tankx,tanky,turPos,gun_power,xlocation,barrier_width,randomHeig
     
 
 def e_fireShell(xy,tankx,tanky,turPos,gun_power,xlocation,barrier_width,randomHeight,ptankx,ptanky):
+
+    currentPower = 1
+    power_found = False
+
+    while not power_found:
+
+        currentPower += 1
+        fire = True
+        startingShell = list(xy)
+
+        while fire:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+
+            print(startingShell[0],startingShell[1])
+            pygame.draw.circle(gameDisplay, green, (startingShell[0],startingShell[1]),5)
+
+            startingShell[0] += (12 - turPos)*2
+                
+        # y = x**2
+            startingShell[1] += int((((startingShell[0] - xy[0]) *0.015/(gun_power/50))**2) - (turPos+turPos/(12-turPos)))
+
+            if startingShell[1] > display_height-ground_height:
+                
+                hit_x = int((startingShell[0]*display_height-ground_height)/startingShell[1])
+                hit_y = int(display_height-ground_height)
+                explosion(hit_x,hit_y)
+                
+                fire = False
+
+            check_x_1 = startingShell[0] <= xlocation + barrier_width
+            check_x_2 = startingShell[0] >= xlocation
+
+            check_y_1 = startingShell[1] <= display_height
+            check_y_2 = startingShell[1] >= display_height - randomHeight
+
+            if check_x_1 and check_x_2 and check_y_1 and check_y_2:
+                
+                  hit_x = int(startingShell[0])
+                  hit_y = int(startingShell[1])
+                  explosion(hit_x,hit_y)
+
+                  if ptankx+15 > hit_x > ptankx-15:
+                      print("Target Acquired")
+                      power_found = True
+                
+                  fire = False
+
+            
     fire = True
-
     startingShell = list(xy)
-
-    
     print("FIRE",xy)
 
     while fire:
@@ -324,7 +370,7 @@ def e_fireShell(xy,tankx,tanky,turPos,gun_power,xlocation,barrier_width,randomHe
         startingShell[0] += (12 - turPos)*2
             
     # y = x**2
-        startingShell[1] += int((((startingShell[0] - xy[0]) *0.015/(gun_power/50))**2) - (turPos+turPos/(12-turPos)))
+        startingShell[1] += int((((startingShell[0] - xy[0]) *0.015/(current_power/50))**2) - (turPos+turPos/(12-turPos)))
 
         if startingShell[1] > display_height-ground_height:
             
